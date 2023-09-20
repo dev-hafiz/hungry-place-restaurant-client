09b5b6018f835f5507f5c2ed3bb0731159a68be4
okshi <johnstarhafiz@gmail.com>
2023-09-20 21:07:18 +0600

    Refector Code

--- src/Pages/Menu/Menu/Menu.jsx
+++ src/Pages/Menu/Menu/Menu.jsx
@@ -1,15 +1,15 @@
 import { Helmet } from "react-helmet-async";
 import Cover from "../../Shared/Cover/Cover";
-
+import coverPhoto from "../../../../src/assets/menu/banner3.jpg";
 const Menu = () => {
   return (
     <section className="h-screen">
-    {/* React Halmet  */}
+      {/* React Halmet  */}
       <Helmet>
         <title>Hungry Place | Our Menu</title>
       </Helmet>
       <div>
-        <Cover title={"Kinds of Food Available"} />
+        <Cover img={coverPhoto} title={"Our Menu"} />
       </div>
     </section>
   );
--- src/Pages/Shared/Cover/Cover.jsx
+++ src/Pages/Shared/Cover/Cover.jsx
@@ -1,17 +1,17 @@
-const Cover = ({ title }) => {
+// eslint-disable-next-line react/prop-types
+const Cover = ({ title, img }) => {
   return (
     <div>
       <div
-        className="hero min-h-screen"
+        className="hero h-[600px]"
         style={{
-          backgroundImage:
-            "url(https://daisyui.com/images/stock/photo-1507358522600-9f71e620c44e.jpg)",
+          backgroundImage: `url("${img}")`,
         }}
       >
         <div className="hero-overlay bg-opacity-60"></div>
         <div className="hero-content text-center text-neutral-content">
           <div className="max-w-md">
-            <h1 className="mb-5 text-4xl text-white font-bold">{title}</h1>
+            <h1 className="mb-5 text-5xl text-white font-bold">{title}</h1>
             <p className="mb-5">
               Provident cupiditate voluptatem et in. Quaerat fugiat ut assumenda
               excepturi exercitationem quasi. In deleniti eaque aut repudiandae
