8b4f56db3ba3080e0c590b8f2a047b741e5c3eaf
okshi <johnstarhafiz@gmail.com>
2023-09-20 21:10:18 +0600

    Refector Code

--- src/Pages/Shared/MenuItems/MenuItem.jsx
+++ src/Pages/Shared/MenuItems/MenuItem.jsx
@@ -4,6 +4,12 @@ const MenuItem = ({ item }) => {
 
   return (
     <div className="flex items-center space-x-5 space-y-6 justify-between ">
+      <img
+        style={{ borderRadius: "0 200px 200px 200px" }}
+        className="w-[118px] h-[104px]"
+        src={image}
+        alt="recipe image"
+      />
       <div>
         <p className="text-xl font-medium">{name} --------</p>
         <p>{recipe}</p>
