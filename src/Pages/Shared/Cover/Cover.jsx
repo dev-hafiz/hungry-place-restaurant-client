329eed1d539bca82d712aa99db6953c50cb0c3a5
okshi <johnstarhafiz@gmail.com>
2023-10-09 19:10:28 +0600

    Working on cover section to design the background opecity

--- src/Pages/Shared/Cover/Cover.jsx
+++ src/Pages/Shared/Cover/Cover.jsx
@@ -13,8 +13,8 @@ const Cover = ({ title, img }) => {
         <div className="hero h-[600px]">
           <div className="hero-overlay bg-opacity-60"></div>
           <div className="hero-content text-center text-neutral-content">
-            <div className="max-w-md">
-              <h1 className="mb-5 text-5xl text-white font-bold">{title}</h1>
+            <div className=" bg-black py-16 bg-opacity-40 px-28 w-2/4	">
+              <h1 className="mb-5 text-5xl  text-white font-bold">{title}</h1>
               <p className="mb-5 text-white">
                 Provident cupiditate voluptatem et in. Quaerat fugiat ut
                 assumenda excepturi exercitationem quasi. In deleniti eaque aut
