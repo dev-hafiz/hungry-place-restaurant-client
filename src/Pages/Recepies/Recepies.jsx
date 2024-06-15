bbb32a4770190c6a87e9d799cf24bc350e765f87
okshi <johnstarhafiz@gmail.com>
2024-06-16 00:11:03 +0600

    Complete food card design

--- src/Pages/Recepies/Recepies.jsx
+++ src/Pages/Recepies/Recepies.jsx
@@ -1,8 +1,8 @@
+/* eslint-disable react/no-unknown-property */
 import SectionTitle from "../../Components/SectionTitle/SectionTitle";
 import { CiSearch } from "react-icons/ci";
 import "./Recepies.css";
-import { IoHeartOutline } from "react-icons/io5";
-import { IoIosArrowForward } from "react-icons/io";
+import { CiShoppingCart } from "react-icons/ci";
 
 const Recepies = () => {
   return (
@@ -80,93 +80,25 @@ const Recepies = () => {
           </div>
           <div className=" col-span-12 md:col-span-9">
             <div className="right-area flex flex-wrap items-center">
-              <div className="item-card">
-                <div className="heart-btn">
-                  <IoHeartOutline />
-                </div>
-                <div className="card-image">
+              {/* food card  */}
+              <div class="card">
+                <div class="card__image">
                   <img
-                    src="https://i.ibb.co/CMbvnmb/cup-prev-ui.png"
-                    alt="food card"
+                    src="https://images.pexels.com/photos/1640777/pexels-photo-1640777.jpeg?auto=compress&cs=tinysrgb&dpr=1&w=500"
+                    alt="Salad"
                   />
                 </div>
-                <div className="card-description">
-                  <h3 className="card-title">Morning Set</h3>
-                  <p className="card-info">
-                    Set of coffee and chocolate cookies as a top tier among our
-                    customers and a perfect way to start your day.
-                  </p>
-                </div>
-                <div className="card-sub-des flex items-center gap-1">
-                  <span>Ingredients:</span>
-                  <div className="card-action-one">sugar</div>
-                  <div className="card-action-two">cheam</div>
-                  <div className="card-action-three">jam</div>
-                </div>
-                <div className="flex items-center mt-2 justify-between">
-                  <p className="card-price"> $16</p>
-                  <button className="card-btn flex items-center gap-1">
-                    Make an order <IoIosArrowForward />
-                  </button>
-                </div>
-              </div>
-              <div className="item-card">
-                <div className="heart-btn">
-                  <IoHeartOutline />
-                </div>
-                <div className="card-image">
-                  <img
-                    src="https://i.ibb.co/CMbvnmb/cup-prev-ui.png"
-                    alt="food card"
-                  />
-                </div>
-                <div className="card-description">
-                  <h3 className="card-title">Morning Set</h3>
-                  <p className="card-info">
-                    Set of coffee and chocolate cookies as a top tier among our
-                    customers and a perfect way to start your day.
-                  </p>
-                </div>
-                <div className="card-sub-des flex items-center gap-1">
-                  <span>Ingredients:</span>
-                  <div className="card-action-one">sugar</div>
-                  <div className="card-action-two">cheam</div>
-                  <div className="card-action-three">jam</div>
-                </div>
-                <div className="flex items-center mt-2 justify-between">
-                  <p className="card-price"> $16</p>
-                  <button className="card-btn flex items-center gap-1">
-                    Make an order <IoIosArrowForward />
-                  </button>
-                </div>
-              </div>
-              <div className="item-card">
-                <div className="heart-btn">
-                  <IoHeartOutline />
-                </div>
-                <div className="card-image">
-                  <img
-                    src="https://i.ibb.co/CMbvnmb/cup-prev-ui.png"
-                    alt="food card"
-                  />
-                </div>
-                <div className="card-description">
-                  <h3 className="card-title">Morning Set</h3>
-                  <p className="card-info">
-                    Set of coffee and chocolate cookies as a top tier among our
-                    customers and a perfect way to start your day.
-                  </p>
-                </div>
-                <div className="card-sub-des flex items-center gap-1">
-                  <span>Ingredients:</span>
-                  <div className="card-action-one">sugar</div>
-                  <div className="card-action-two">cheam</div>
-                  <div className="card-action-three">jam</div>
-                </div>
-                <div className="flex items-center mt-2 justify-between">
-                  <p className="card-price"> $16</p>
-                  <button className="card-btn flex items-center gap-1">
-                    Make an order <IoIosArrowForward />
+                <div class="card__info">
+                  <div class="car__info--title">
+                    <h3>Salad</h3>
+                    <p>Fresh & sweet</p>
+                  </div>
+
+                  <div class="card__info--price">
+                    <p>$ 5</p>
+                  </div>
+                  <button className="cart-btn">
+                    Order <CiShoppingCart />
                   </button>
                 </div>
               </div>
