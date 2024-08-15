01dd3f71a54d703285756670186a34025462226a
dev-hafiz <johnstarhafiz@gmail.com>
2024-08-15 11:03:50 +0600

    Ready to deploy client on firebase

--- src/firebase/firebase.config.js
+++ src/firebase/firebase.config.js
@@ -1,10 +1,11 @@
+// Your web app's Firebase configuration
 const firebaseConfig = {
-  apiKey: "AIzaSyCCw68modH2ZZ21ygVodAXZ7_xNPZoLSE0",
-  authDomain: "hubgry-place.firebaseapp.com",
-  projectId: "hubgry-place",
-  storageBucket: "hubgry-place.appspot.com",
-  messagingSenderId: "16941292331",
-  appId: "1:16941292331:web:ec16123ffe1b35d8ddc36e",
+  apiKey: import.meta.env.VITE_apiKey,
+  authDomain: import.meta.env.VITE_authDomain,
+  projectId: import.meta.env.VITE_projectId,
+  storageBucket: import.meta.env.VITE_storageBucket,
+  messagingSenderId: import.meta.env.VITE_messagingSenderId,
+  appId: import.meta.env.VITE_appId,
 };
 
 export default firebaseConfig;
